3c85c0202b249f0a4389fcfea155dab269defebd
MenousMeriem <meriemmenous1@gmail.com>
2023-04-04 23:32:26 +0200

    7 commit

--- src/Component/Pagination/Pagination.jsx
+++ src/Component/Pagination/Pagination.jsx
@@ -1,21 +1,12 @@
 import "./Pagination.css"
 const Pagination = () => {
     return ( 
-<<<<<<< HEAD
         <div className="btn-group">
           <button className="btn btn-active">1</button>
           <button className="btn">2</button>
           <button className="btn">3</button>
           <button className="btn">4</button>
       </div>
-=======
-<div className="btn-group">
-  <button className="btn btn-active">1</button>
-  <button className="btn">2</button>
-  <button className="btn">3</button>
-  <button className="btn">4</button>
-</div>
->>>>>>> 824bd5cf4e3cd4030754e5b23580bef135f4b7e3
      );
 }
  
