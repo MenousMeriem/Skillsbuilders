e6a756e2a557921612e8b053433d065273891086
Madjda-Rezig <rezigmadjda@yahoo.com>
2023-04-05 13:19:02 +0100

    calendrier

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Accueil from "./Pages/Accueil.jsx"
 import Formations from "./Pages/Formations.jsx"
 import Projets from "./Pages/Projets.jsx"
 import Carriere from "./Pages/Carriere.jsx"
+import Evenements from "./Pages/Evenements.jsx"
 import { 
   createBrowserRouter, 
   createRoutesFromElements, 
@@ -22,8 +23,8 @@ const router = createBrowserRouter(
         <Route path='Formations' element={<Formations/>}/>
         <Route path='Projets' element={<Projets/>}/>
         <Route path='Carriere' element={<Carriere/>}/>
-        <Route path='Evenements' element={<Carriere/>}/>
-        <Route path='Calendrier' element={<Carriere/>}/>
+        <Route path='Evenements' element={<Evenements/>}/>
+        <Route path='Calendrier' element={<Calendrier/>}/>
       </Route >
       
 ));
