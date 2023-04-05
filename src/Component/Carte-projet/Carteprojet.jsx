8bf13807266762a8c3f7bee54b59364959286905
MenousMeriem <meriemmenous1@gmail.com>
2023-04-05 13:29:39 +0200

    8 commit

--- src/Component/Carte-projet/Carteprojet.jsx
+++ src/Component/Carte-projet/Carteprojet.jsx
@@ -7,16 +7,17 @@ function Carteprojet() {
             <div className="card lg:card-side bg-base-100 shadow-xl">
                 
                 <div className="card-body">
-                    <h2 className="card-title text-center">Projets réalisés</h2>
-                    <p className='text text-black'> Bienvenue sur notre plateforme de cours en ligne! <br /> 
+                    <h2 className="card-title  text-black text-center text-4xl leading-10 ">Projets réalisés</h2>
+                    <p className='text-black text-center text-2xl leading-10'> 
+                        Bienvenue sur notre plateforme de cours en ligne! <br /> 
                         Nous sommes ravis de vous offrir une opportunité unique <br />
                         d'apprendre depuis n'importe où et à n'importe quel moment.</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn bg-orange-600 border-transparent	">Découvrire nos projets</button>
+                    <div className="card-actions justify-center">
+                        <button className="btn bg-orange-600 border-transparent">Découvrire nos projets</button>
                     </div>
                 </div>
                 <figure className="">
-                    <img src="./assets/2.png"/>
+                    <img className='w-11/12 h-11/12' src="./assets/2.png"/>
                 </figure>
             </div>
         </div>
