2bc0ee2548af72008b834dc0c5e0b11ae7f4af90
Madjda-Rezig <rezigmadjda@yahoo.com>
2023-04-05 23:06:04 +0100

    actioncard2

--- src/Pages/Formations.jsx
+++ src/Pages/Formations.jsx
@@ -1,11 +1,13 @@
 import Footer from "../Component/Footer/Footer.jsx"
 import Pagination from "../Component/Pagination/Pagination.jsx"
 import Recherche from "../Component/Recherche/Recherche.jsx"
+import Actioncard from "../Component/Actioncard/Actioncard.jsx"
 
 const Formations = () => {
     return ( 
        <div className="flex justify-center flex-col items-center">
-        <h1>jkdfhdlfjdlfdlfjdklj</h1>
+        
+        <Actioncard textaction="Dans quel domaine souhaitez-vous vous former ?"/>
         <Recherche />
         <Pagination/>
         <Footer/>
--- src/Pages/Projets.jsx
+++ src/Pages/Projets.jsx
@@ -10,7 +10,7 @@ const Projets = () => {
 projets passionnants que nous avons réalisés.Nous avons travaillé dur pour créer des solutions innovantes et répondre aux besoins de nos clients."/>
         <Bigcard bg="#364DC8" bgColor="#364DC8" cardtitle="Bibliothèque en ligne " cardtext="Bienvenue dans notre bibliothèque en ligne! Nous sommes ravis de vous offrir un accès facile à une vaste collection de livres numériques, d'articles de journaux, de magazines, de documents académiques et plus encore."/>
         <Bigcard bg="#36BFC8" bgColor="#36BFC8" cardtitle="Gestion de cabinet " cardtext="Bienvenue dans notre bibliothèque en ligne! Nous sommes ravis de vous offrir un accès facile à une vaste collection de livres numériques, d'articles de journaux, de magazines, de documents académiques et plus encore."/>
-        <Bigcard bg="#BADDB7" cardtitle="Agence de voyage" cardtext="Bienvenue dans notre bibliothèque en ligne! Nous sommes ravis de vous offrir un accès facile à une vaste collection de livres numériques, d'articles de journaux, de magazines, de documents académiques et plus encore."/>
+        <Bigcard bg="#BADDB7" bgColor="#BADDB7" cardtitle="Agence de voyage" cardtext="Bienvenue dans notre bibliothèque en ligne! Nous sommes ravis de vous offrir un accès facile à une vaste collection de livres numériques, d'articles de journaux, de magazines, de documents académiques et plus encore."/>
         <Pagination/>
         
         <Footer/>
