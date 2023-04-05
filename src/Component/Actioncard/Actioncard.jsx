e5922d976b32f802c20fd0bbb4dd1e5b5e99db10
Madjda-Rezig <rezigmadjda@yahoo.com>
2023-04-05 23:36:56 +0100

    badge

--- src/Component/Actioncard/Actioncard.jsx
+++ src/Component/Actioncard/Actioncard.jsx
@@ -6,16 +6,10 @@ const Actioncard = ({textaction}) => {
           <h6 className="texte-action">{textaction}</h6> 
         </div>
 
-        <div className="cardaction w-96 bg-base-100 shadow-xl">
-           <div className="card-body">
-              <div className="card-actions justify-end">
-                   <button className="btnaction btn-square btn-xs">
-                   <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
-                   </button>
-              </div>
-              <p>HTML</p>
-              </div>
-           </div>
+        <div className="badge badge-outline ">HTML</div>
+             <div className="badge badge-primary badge-outline">CSS</div>
+             <div className="badge badge-secondary badge-outline">JS</div>
+             
         </div>   
      );
 }
